b86d9ed63e5300591cdc6f7dd0a530b7bf42570f
Rafael Aragão <rafaelfaragao@gmail.com>
2021-06-03 22:48:07 -0300

    Ajustando visualização vindo do back e logica e tratamento das datas

--- instructors.js
+++ instructors.js
@@ -1,5 +1,26 @@
 const fs = require('fs')
 const data = require("./data.json")
+const { age } = require("./utils")
+
+//show
+exports.show = function(req,res) {
+    //req.params
+    const {id} = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+    if(!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services:foundInstructor.services.split(","),
+        created_at:""
+    }
+
+    return res.render("instructors/show", {instructor})
+}
 
 //create - post
 exports.post = function(req,res) {
