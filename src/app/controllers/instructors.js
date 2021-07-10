359c88e807c44f5d7db253d767ed22dabc028565
Rafael Aragão <rafaelfaragao@gmail.com>
2021-07-09 23:55:47 -0300

    adicionando rotina de inserir e buscar todos os dados dos instructors no controller

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,8 +1,15 @@
 const { age, date } = require("../../lib/utils")
+const db = require('../../config/db')
 
 module.exports = {
     index(req,res){
-        return res.render("instructors/index", {instructors})
+
+        db.query(`SELECT * FROM instructors`, function(err, results){
+            if(err) return res.send("Database error!")
+
+            return res.render("instructors/index", {instructors: results.rows})
+        })
+
     },
     create(req,res){
         return res.render('instructors/create')
@@ -16,9 +23,33 @@ module.exports = {
             }
         }
 
-        let {name, avatar_url, birth, gender, services} = req.body
+        const query = `
+            INSERT INTO instructors (
+                name,
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id
+        `
         
-        return
+        const values = [
+            req.body.name,
+            req.body.avatar_url,
+            req.body.gender,
+            req.body.services,
+            date(req.body.birth).iso,
+            date(Date.now()).iso
+        ]
+
+        db.query(query, values, function(err, results){
+           if(err) return res.send("Databse Error!")
+
+           return res.redirect(`/instructors/${results.rows[0].id}`)
+        })
+
     },
     show(req,res){
         return
